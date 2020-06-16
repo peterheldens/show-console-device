fcf66dafca9a3d72358b8af0dab89b5fad57b2e7
Make Code Innovate <66961990+makecodeinnovate@users.noreply.github.com>
2020-06-16 17:02:01 +0200

    added show icon

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
 basic.forever(function () {
     serial.writeNumbers([input.acceleration(Dimension.X), input.acceleration(Dimension.Y), input.acceleration(Dimension.Z)])
     serial.writeValue("x", input.acceleration(Dimension.X))
