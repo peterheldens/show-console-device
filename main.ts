b34422ac38902f2d641e60bb06160719a4cfb358
Make Code Innovate <66961990+makecodeinnovate@users.noreply.github.com>
2020-06-16 15:27:21 +0200

    changed order

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     serial.writeNumbers([input.acceleration(Dimension.X), input.acceleration(Dimension.Y), input.acceleration(Dimension.Z)])
-    serial.writeValue("x", input.acceleration(Dimension.X))
     serial.writeValue("a.x", input.acceleration(Dimension.X))
     serial.writeValue("a.y", input.acceleration(Dimension.Y))
+    serial.writeValue("x", input.acceleration(Dimension.X))
 })
