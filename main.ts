72c0dac3059d103685f772ebc590ed2fc11da0c9
Peter Heldens <peter.heldens@live.nl>
2020-06-16 09:56:51 +0200

    Show different streams and combine multiple sensors in one stream

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function () {
     serial.writeNumbers([input.acceleration(Dimension.X), input.acceleration(Dimension.Y), input.acceleration(Dimension.Z)])
+    serial.writeValue("x", input.acceleration(Dimension.X))
+    serial.writeValue("a.x", input.acceleration(Dimension.X))
+    serial.writeValue("a.y", input.acceleration(Dimension.Y))
 })
